44fbaf68fe31b20ff0b0e53eedcc79a5c8255b4e
Jaspreet Singh Nahal <jaspreetnahal100@gmail.com>
2025-04-27 14:58:35 +0530

    Denoted

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -41,7 +41,7 @@ const Hero: React.FC = () => {
               href="https://www.linkedin.com/in/jaspreet-nahal/overlay/1739806819683/single-media-viewer/?profileId=ACoAAEem9oUBYcGhn-RPsgF959px8d9U4zFKNlc"
               className="px-8 py-3 bg-blue-600 hover:bg-blue-700 text-white font-medium rounded-full transition-all duration-300 transform hover:scale-105 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50"
             >
-              My Resume
+              My Resumé
             </a>
           </div>
         </div>
